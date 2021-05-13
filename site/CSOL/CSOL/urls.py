cecd6610fa00901d4f92b6087001e9249deddc93
DemLoveSky (CSU) <dem.loginov@gmail.com>
2021-05-13 11:19:50 +0500

    test

--- site/CSOL/CSOL/urls.py
+++ site/CSOL/CSOL/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
     path('games/', include('games.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- site/CSOL/accounts/urls.py
+++ site/CSOL/accounts/urls.py
@@ -1,7 +1,2 @@
 from django.contrib.auth import views
 from django.urls import path
-
-app_name = 'accounts'
-urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name='accounts/registration/login.html'), name='login'),
-]
