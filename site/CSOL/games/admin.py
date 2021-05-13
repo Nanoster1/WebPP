baf497c37449df6de377ba5576a27877dc3e11c5
DemLoveSky <dem.loginov@gmail.com>
2021-05-14 02:14:28 +0500

    Refactor models in games

--- site/CSOL/games/admin.py
+++ site/CSOL/games/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Game
+from .models import *
 
+admin.site.register(CategoryGame)
+admin.site.register(TagsGame)
 admin.site.register(Game)
+admin.site.register(CreatorGame)
+admin.site.register(CreatorCompany)
