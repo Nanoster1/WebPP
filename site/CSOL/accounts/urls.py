2abb1dc2daec24bb88e6b5e6445506137337d3f0
DemLoveSky <dem.loginov@gmail.com>
2021-05-15 21:19:23 +0500

    Add views in accounts (login, register)

--- site/CSOL/accounts/urls.py
+++ site/CSOL/accounts/urls.py
@@ -1,2 +1,10 @@
 from django.contrib.auth import views
 from django.urls import path
+
+from .views import login, register
+
+app_name = 'accounts'
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+]
