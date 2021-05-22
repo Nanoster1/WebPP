90f8f2b6c07b67c2313134cbd887b406f2705535
Slava <sh-e-95@mail.ru>
2021-05-23 00:06:43 +0500

    add script

---
+++ site/CSOL/accounts/templates/Profile/Open menu.js
@@ -0,0 +1,7 @@
+let Btn = document.getElementById('AddFriendBTN');
+let list = document.getElementById('block-list');
+function handleButtonClick()
+{
+    !list.hidden;
+}
+Btn.onclick += handleButtonClick;
